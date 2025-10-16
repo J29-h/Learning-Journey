c0744848b126586746fa8c9bae10b61e947493a3
Jana Malibari <Konderyano.0o0@hotmail.com>
2025-10-16 10:22:17 +0300

    Initial Commit

---
+++ Learning Journey/Learning_JourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_JourneyApp.swift
+//  Learning Journey
+//
+//  Created by Jana Abdulaziz Malibari on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Learning_JourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Learning JourneyTests/Learning_JourneyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_JourneyTests.swift
+//  Learning JourneyTests
+//
+//  Created by Jana Abdulaziz Malibari on 16/10/2025.
+//
+
+import Testing
+@testable import Learning_Journey
+
+struct Learning_JourneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
